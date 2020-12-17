c814cf5dfa230ca3f5e3da0de9884db0d3a1bb29
Тихон Воробьев <tihonvorobev@Ikhon.local>
2020-12-17 20:03:30 +0300

    cr1

--- args.py
+++ args.py
@@ -4,7 +4,7 @@ parser = argparse.ArgumentParser(description='Hello DB web application')
 parser.add_argument('--pg-host', help='PostgreSQL host name', default='localhost')
 parser.add_argument('--pg-port', help='PostgreSQL port', default=5432)
 parser.add_argument('--pg-user', help='PostgreSQL user', default='postgres')
-parser.add_argument('--pg-password', help='PostgreSQL password', default='')
+parser.add_argument('--pg-password', help='PostgreSQL password', default='postgres')
 parser.add_argument('--pg-database', help='PostgreSQL database', default='postgres')
 
 _args = parser.parse_args()
--- model.py
+++ model.py
@@ -1,18 +1,19 @@
 # encoding: UTF-8
 # В этом файле реализованы Data Access Objects в виде классов Peewee ORM
 from peewee import *
-
 from connect import getconn
 from connect import LoggingDatabase
 from args import *
 
-db = PostgresqlDatabase(args().pg_database, user=args().pg_user, host=args().pg_host, password=args().pg_password)
+#db = PostgresqlDatabase(args().pg_database, user=args().pg_user, host=args().pg_host, password=args().pg_password)
+#наверное не надо создавать новый пулл сессий
+db = getconn()
 #db = LoggingDatabase(args())
 
 
 # Классы ORM модели.
 class PlanetEntity(Model):
-    id = IntegerField()
+    id = IntegerField() #AutoField() не будет ругаться на то что нет primary key?
     distance = DecimalField()
     name = TextField()
 
@@ -22,7 +23,7 @@ class PlanetEntity(Model):
 
 
 class FlightEntity(Model):
-    id = IntegerField()
+    id = IntegerField() #AutoField()
     date = DateField()
     available_seats = IntegerField()
     planet = ForeignKeyField(PlanetEntity, related_name='flights')
@@ -33,9 +34,12 @@ class FlightEntity(Model):
 
 
 class PriceEntity(Model):
-    id = IntegerField()
-    fare_code = IntegerField()
+    id = IntegerField() #AutoField()
+    planet = ForeignKeyField(PlanetEntity, related_name='price')
+    #добавить ограничение, точно не знаю как правильно
+    fare_code = IntegerField(constraints=[Check('fare_code BETWEEN 1 AND 10')])
     price = IntegerField()
+    #UNIQUE(planet_id, fare_code) нет проверки
 
     class Meta:
         database = db
@@ -43,7 +47,7 @@ class PriceEntity(Model):
 
 
 class TicketEntity(Model):
-    id = IntegerField()
+    id = IntegerField() #AutoField()
     price = ForeignKeyField(PriceEntity)
     flight = ForeignKeyField(FlightEntity)
     discount = DecimalField()
@@ -59,9 +63,15 @@ class TicketEntity(Model):
 
     # Устанавливает размер скидки на билет
     def set_discount(self, discount):
-        with getconn() as db:
+        db = getconn()
+        cur = db.cursor()
+        #используем пулл сессий
+        try:
             cur = db.cursor()
             cur.execute("UPDATE FlightTicket SET discount=%s WHERE id=%s", (discount, self.id))
+            db.commit()
+        finally:
+            db.close()
 
 
 class BookingEntity(Model):
